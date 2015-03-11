11f70ed827280b8825cdcd3566137dcd0999bb21
Kevin Campusano <megakevinx@gmail.com>
2015-03-11 15:07:44 -0400

    Refactored the product item layout.

--- views/index.view.php
+++ views/index.view.php
@@ -1,53 +1,14 @@
+<?php
+
+require "shared/show_product_container.view.php";
+
+?>
+
 <section id="offers">
     <h2>Today's Offers</h2>
-
-    <ul class="product-container">
-        <?php foreach ($context->products_on_sale as $product) { ?>
-            <li class="product">
-                <h3><?= $product->name ?></h3>
-                <div class="picture"><img src="<?= $product->picture ?>" alt="<?= $product->name ?>"/></div>
-                <p><?= $product->description ?></p>
-                <p class="stock"><?= $product->quantity_in_stock ?> left</p>
-                <p class="price">
-                    <span class="old">$ <?= $product->price ?></span>
-                    <span class="new">$ <?= $product->sale_price ?></span>
-                </p>
-                <div class="button">
-                    <form method="POST" action="index.php">
-                        <input type="hidden" id="product_id" name="product_id" value="<?= $product->id ?>" />
-                        <input type="submit" name="add_to_cart_submit" value="Add to cart">
-                    </form>
-                </div>
-            </li>
-        <?php } ?>
-    </ul>
-
-<!--    <pre>-->
-<!--    --><?php //print_r($context->products_on_sale) ?>
-<!--    </pre>-->
+    <?php show_product_container($context->products_on_sale) ?>
 </section>
 <section id="catalog">
     <h2>Product Catalog</h2>
-
-    <ul class="product-container">
-        <?php foreach ($context->products as $product) { ?>
-            <li class="product">
-                <h3><?= $product->name ?></h3>
-                <div class="picture"><img src="<?= $product->picture ?>" alt="<?= $product->name ?>"/></div>
-                <p><?= $product->description ?></p>
-                <p class="stock"><?= $product->quantity_in_stock ?> left</p>
-                <p class="price">$ <?= $product->price ?></p>
-                <div class="button">
-                    <form method="POST" action="index.php">
-                        <input type="hidden" id="product_id" name="product_id" value="<?= $product->id ?>" />
-                        <input type="submit" name="add_to_cart_submit" value="Add to cart">
-                    </form>
-                </div>
-            </li>
-        <?php } ?>
-    </ul>
-
-<!--    <pre>-->
-<!--    --><?php ////echo print_r($context->products) ?>
-<!--    </pre>-->
+    <?php show_product_container($context->products) ?>
 </section>
---
+++ views/shared/show_product_container.view.php
@@ -0,0 +1,34 @@
+<?php
+function show_product_container($products)
+{
+    echo "<ul class='product-container'>";
+    foreach ($products as $product)
+    {
+        echo "<li class='product'>
+                <h3>$product->name</h3>
+                <div class='picture'><img src='$product->picture' alt='$product->name'/></div>
+                <p>$product->description</p>
+                <p class='stock'>$product->quantity_in_stock left</p>";
+
+        if ($product->is_on_sale)
+        {
+            echo "<p class='price'>
+                        <span class='old'>$ $product->price</span>
+                        <span class='new'>$ $product->sale_price</span>
+                    </p>";
+        }
+        else
+        {
+            echo "<p class='price'>$ $product->price</p>";
+        }
+
+        echo "<div class='button'>
+                    <form method='POST' action='index.php'>
+                        <input type='hidden' id='product_id' name='product_id' value='$product->id' />
+                        <input type='submit' name='add_to_cart_submit' value='Add to cart'>
+                    </form>
+                </div>
+            </li>";
+    }
+    echo "</ul>";
+}
