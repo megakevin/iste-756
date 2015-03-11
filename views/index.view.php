9d3360a60c43142dbf57b0f7d82ff280739fea71
Kevin Campusano <megakevinx@gmail.com>
2015-03-11 15:23:48 -0400

    Refactored shou_product_container. Now its a proper view file that is invoked via an include. Boom

--- views/index.view.php
+++ views/index.view.php
@@ -1,14 +1,16 @@
-<?php
-
-require "shared/show_product_container.view.php";
-
-?>
-
 <section id="offers">
     <h2>Today's Offers</h2>
-    <?php show_product_container($context->products_on_sale) ?>
+    <?php
+        $products_to_show = $context->products_on_sale;
+        include "shared/product_container.view.php";
+    ?>
+    <?php //show_product_container($context->products_on_sale) ?>
 </section>
 <section id="catalog">
     <h2>Product Catalog</h2>
-    <?php show_product_container($context->products) ?>
+    <?php
+        $products_to_show = $context->products;
+        include "shared/product_container.view.php";
+    ?>
+    <?php //show_product_container($context->products) ?>
 </section>
