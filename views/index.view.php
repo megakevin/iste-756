0310b76f71cea37d6d2675890fe3e01f86ca9a00
Kevin Campusano <megakevinx@gmail.com>
2015-03-11 22:40:27 -0400

    Yet another refactor of the whole show_product thing.

--- views/index.view.php
+++ views/index.view.php
@@ -1,16 +1,16 @@
+<?php
+    require_once "shared/show_product_container.view.php";
+?>
+
 <section id="offers">
     <h2>Today's Offers</h2>
     <?php
-        $products_to_show = $context->products_on_sale;
-        include "shared/product_container.view.php";
+        show_product_container($context->products_on_sale);
     ?>
-    <?php //show_product_container($context->products_on_sale) ?>
 </section>
 <section id="catalog">
     <h2>Product Catalog</h2>
     <?php
-        $products_to_show = $context->products;
-        include "shared/product_container.view.php";
+        show_product_container($context->products);
     ?>
-    <?php //show_product_container($context->products) ?>
 </section>
